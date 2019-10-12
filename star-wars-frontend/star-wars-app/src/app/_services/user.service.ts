21afa32a0eea163493bfe535174ae9752c6e63ea
bonfantimariano <mariano@holidog.com>
2019-10-12 19:53:08 +0200

    User registration and login added (frontend)

---
+++ star-wars-frontend/star-wars-app/src/app/_services/user.service.ts
@@ -0,0 +1,31 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppConfig } from '../app.config';
+
+
+import { User } from '../_models';
+
+@Injectable()
+export class UserService {
+    constructor(private http: HttpClient, private appConfig: AppConfig) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${this.appConfig.apiUrl}/users`);
+    }
+
+    getById(id: number) {
+        return this.http.get(`${this.appConfig.apiUrl}/users/` + id);
+    }
+
+    register(user: User) {
+        return this.http.post(`${this.appConfig.apiUrl}/users/register`, user);
+    }
+
+    update(user: User) {
+        return this.http.put(`${this.appConfig.apiUrl}/users/` + user.id, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${this.appConfig.apiUrl}/users/` + id);
+    }
+}
---
+++ star-wars-frontend/star-wars-app/src/app/app.config.ts
@@ -0,0 +1,5 @@
+﻿import { environment } from '../environments/environment';
+
+export class AppConfig {
+	public readonly apiUrl = environment.apiUrl;
+}
